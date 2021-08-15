9cc07959e9cb13bfc074ea91c10e5e2f9cfc6785
noruzzamanrubel <noruzzamanrubel@gmail.com>
2021-08-15 16:38:44 +0600

    remove comment code

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -3,95 +3,97 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Models\Todo;
 use Auth;
+use Illuminate\Http\Request;
 
-class TodoController extends Controller
-{
+class TodoController extends Controller {
 
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-    
-
-    public  function index()
-    {
-
-        $todo = auth()->user()->todos()->orderBy('completed')->get();
-        // $todo= Todo::orderBy('completed')->get();
-        return view('Todo.all-todo', compact('todo'));
+    public function __construct() {
+        $this->middleware( 'auth' );
     }
 
-    public  function create()
-    {
-        return view('Todo.create');
+    public function index() {
+
+        $todo = auth()->user()->todos()->orderBy( 'completed' )->get();
+
+        return view( 'Todo.all-todo', compact( 'todo' ) );
     }
 
-    public  function store(Request $request)
-    {
-        $validated = $request->validate([
-            'title' => 'required|unique:todos|max:255',
+    public function create() {
+        return view( 'Todo.create' );
+    }
+
+    /**
+     * @param Request $request
+     */
+    public function store( Request $request ) {
+        $validated = $request->validate( [
+            'title'       => 'required|unique:todos|max:255',
             'description' => 'required|unique:todos|max:255',
-        ]);
+        ] );
 
-        // $title = new Todo();
-        // $title->title = $request->title;
-        // $title->save();
+        auth()->user()->todos()->create( $request->all() );
 
-        auth()->user()->todos()->create($request->all());
-
-        // Todo::insert([
-        //     'title'=>$request->title,
-        // ]);
-
-        return redirect()->route('all');
+        return redirect()->route( 'all' );
     }
 
-    public function show(Todo $todo)
-    {
-        return view('todo.show', compact('todo'));
+    /**
+     * @param Todo $todo
+     */
+    public function show( Todo $todo ) {
+        return view( 'todo.show', compact( 'todo' ) );
     }
 
-    public  function edit(Todo $todo)
-    {
-        return view('Todo.edit', compact('todo'));
+    /**
+     * @param Todo $todo
+     */
+    public function edit( Todo $todo ) {
+        return view( 'Todo.edit', compact( 'todo' ) );
     }
 
-    public  function update(Request $request, Todo $todo)
-    {
+    /**
+     * @param Request $request
+     * @param Todo $todo
+     */
+    public function update( Request $request, Todo $todo ) {
 
-        $validated = $request->validate([
-            'title' => 'required|unique:todos|max:255',
+        $validated = $request->validate( [
+            'title'       => 'required|unique:todos|max:255',
             'description' => 'required|unique:todos|max:255',
-        ]);
+        ] );
 
-        $todo->update([
-            'title' => $request->title,
+        $todo->update( [
+            'title'       => $request->title,
             'description' => $request->description,
-        ]);
-        return redirect()->route('all');
+        ] );
+        return redirect()->route( 'all' );
     }
 
-    public function complete(Todo $todo)
-    {
-        $todo->update([
+    /**
+     * @param Todo $todo
+     */
+    public function complete( Todo $todo ) {
+        $todo->update( [
             'completed' => true,
-        ]);
+        ] );
         return redirect()->back();
     }
 
-    public function incomplete(Todo $todo)
-    {
-        $todo->update([
+    /**
+     * @param Todo $todo
+     */
+    public function incomplete( Todo $todo ) {
+        $todo->update( [
             'completed' => false,
-        ]);
+        ] );
         return redirect()->back();
     }
 
-    public function delete(Todo $todo)
-    {
+    /**
+     * @param Todo $todo
+     */
+    public function delete( Todo $todo ) {
         $todo->delete();
         return redirect()->back();
     }
